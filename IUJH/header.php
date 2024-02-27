d27fd8b7218d0f3607946b5dffe2082338ce9582
KuberKunal <128916310+KuberKunal@users.noreply.github.com>
2024-02-27 21:31:30 +0530

    ICFAI HACKTHON

---
+++ IUJH/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-com</title>
+    <!-- custom css -->
+    <link rel="stylesheet" href="assets/style.css">
+    <!-- tailwind css -->
+    <link rel="stylesheet" hret="assets/tailwind.min.css">
+    <!-- <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script> -->
+    <!-- font awesome icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous" />
+</head>
+<body>
+
+<header class="header">
+    <div class="container">
+      <a href="#" class="logo">
+        <span class="logo-text">E-com</span>
+      </a>
+      <nav class="nav-menu">
+        <a href="#" class="nav-link">First Link</a>
+        <a href="#" class="nav-link">Second Link</a>
+        <a href="#" class="nav-link">Third Link</a>
+        <a href="#" class="nav-link">Fourth Link</a>
+      </nav>
+      <a href="login.php">
+            <button class="header-button">Button</button>
+        </a>
+    </div>
+  </header>
+
+<main id="main-site">
---
+++ IUJH/index.php
@@ -0,0 +1,12 @@
+<?php
+    ob_start();
+    // include header.php file
+    include ('header.php');
+?>
+
+
+
+<?php
+// include footer.php file
+include ('footer.php');
+?>
